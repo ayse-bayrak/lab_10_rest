4578599cbc5544597b8f7dc0031aad876cf7529b
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-20 14:22:05 +0300

    TeacherController endpoints are created

---
+++ src/main/java/com/cydeo/controller/TeacherController.java
@@ -0,0 +1,61 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.TeacherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/teacher")
+public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+    /*
+      Endpoint: /api/v1/teacher
+
+      JSON Response Body:
+      <teachers data>
+   */
+    @GetMapping
+    public List<TeacherDTO> getTeachers() {
+        return teacherService.findAll();
+    }
+        /*
+           Endpoint: /api/v1/teacher/{username}
+           HTTP Status Code: 200
+
+           JSON Response Body:
+           "success": true
+           "message": "Teacher is successfully retrieved."
+           "code":200
+           "data":<teacher data>
+        */
+    @GetMapping("{username}")
+    public ResponseEntity<ResponseWrapper> getTeacherByUserName(@PathVariable ("username") String username) {
+        return ResponseEntity
+                .ok(new ResponseWrapper("Teacher is successfully retrieved.", teacherService.findByUsername(username)));
+    }
+
+       /*
+           Endpoint: /api/v1/teacher
+           HTTP Status Code: 201
+           Custom Response Header: "teacherUsername", <created username>
+
+           JSON Response Body:
+           "success": true
+           "message": "Teacher is successfully created."
+           "code":201
+           "data":<created teacher data>
+     */
+
+}
