9409d6e3acab98dd163527282748a62e33d3db00
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-24 12:22:32 +0300

    ParentController is fixed

--- src/main/java/com/cydeo/controller/ParentController.java
+++ src/main/java/com/cydeo/controller/ParentController.java
@@ -1,12 +1,16 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ParentDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.ParentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,12 +22,24 @@ public class ParentController {
         this.parentService = parentService;
     }
 
-    @GetMapping("/parent")
-    public ResponseEntity<ResponseWrapper> getParents() {
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+
+        //find all the parents --> List<ParentDTO>
+        List<ParentDTO> parents = parentService.findAll();
+        //Design my custom json response --> ResponseWrapper
+        ResponseWrapper responseWrapper = ResponseWrapper.builder()
+                .success(true)
+                .message("Parents are successfully retrieved.")
+                .code(HttpStatus.OK.value())
+                .data(parents).build();
+        //Return JSON Body with
+        //status code : 200
+        //header : Parents - Returned
         return ResponseEntity
-                .status(HttpStatusCode.valueOf(200))
-                .header("Parents", "Returned")
-                .body(new ResponseWrapper("Parents are successfully retrieved. ", parentService.findAll()));
+                .status(HttpStatus.OK)
+                .header("Parents","Returned")
+                .body(responseWrapper);
     }
 
      /*
