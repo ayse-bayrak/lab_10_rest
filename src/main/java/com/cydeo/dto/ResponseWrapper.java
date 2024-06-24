1b5b55b80ccf5b8ea8f54388ba7c2f046fd4b11f
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-24 12:23:10 +0300

    ResponseWrapper is fixed

--- src/main/java/com/cydeo/dto/ResponseWrapper.java
+++ src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -1,31 +1,31 @@
 package com.cydeo.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.ObjectError;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder //= @No + @All --> builder helps to create a multiple type of constructor even remotely, without creating hard code
 public class ResponseWrapper {
 
     private boolean success;
     private String message;
     private Integer code;
     private Object data;
-
-    public ResponseWrapper(String message,Object data){
+ //we don't need t create any constructor because we use @Builder
+    public ResponseWrapper(String message,Object data, Integer code){
         this.message = message;
         this.data=data;
-        this.code= HttpStatus.OK.value();
+        this.code= code;
         this.success=true;
     }
 
-    public ResponseWrapper(String message){
+    public ResponseWrapper(String message, Integer code){
         this.message=message;
-        this.code=HttpStatus.OK.value();
+        this.code=code;
         this.success=true;
     }
 
