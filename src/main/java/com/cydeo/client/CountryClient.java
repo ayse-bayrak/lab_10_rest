fb69c4dbf7f9e566220857893e463d8e75737422
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-30 23:55:03 +0300

    CountryClient is added

--- src/main/java/com/cydeo/client/CountryClient.java
+++ src/main/java/com/cydeo/client/CountryClient.java
@@ -3,14 +3,13 @@ package com.cydeo.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
 
 //define Feign Client to access RestCountries API
 @FeignClient(name = "FlagClient", url = "https://restcountries.com/v3.1/name")
-public interface FlagClient {
+public interface CountryClient {
 
     @GetMapping("/{name}")
     List<Map<String, Object>> getFlag(@PathVariable("name") String country);
